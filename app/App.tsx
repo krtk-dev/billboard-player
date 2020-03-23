2ee775c56d7b11d4cf9e3470a911d4d3fb1ae1ed
hyun <coderhyun476@gmail.com>
2020-03-23 23:22:10 +0900

    FCM

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppContainer from './src/screens'
 import BottomBannerAds from './src/components/View/BottomBannerAds';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+
+  useEffect(() => {
+    try {
+      messaging().registerForRemoteNotifications();
+    } catch (error) {
+
+    }
+  }, [])
+
   return (
     <>
       <AppContainer />

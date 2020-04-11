f063de23450494879c965633cd39b166ee2e9f79
hyun <coderhyun476@gmail.com>
2020-04-11 17:30:17 +0900

    페이스북 sdk logger

--- app/android/app/src/main/java/com/koreanthinker/billboard/MainActivity.java
+++ app/android/app/src/main/java/com/koreanthinker/billboard/MainActivity.java
@@ -1,5 +1,7 @@
 package com.koreanthinker.billboard;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -7,22 +9,20 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "app";
-  }
+    @Override
+    protected String getMainComponentName() {
+        return "app";
+    }
 
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-   return new ReactActivityDelegate(this, getMainComponentName()) {
-     @Override
-     protected ReactRootView createRootView() {
-      return new RNGestureHandlerEnabledRootView(MainActivity.this);
-     }
-   };
- }
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
+
+
 }

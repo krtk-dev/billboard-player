b48603b40326d1a02c24cc809d7ed02f8a3b7796
hyun <coderhyun476@gmail.com>
2020-05-26 23:23:30 +0900

    종료시 전면 광고

--- app/src/screens/HomeScreen/index.tsx
+++ app/src/screens/HomeScreen/index.tsx
@@ -1,12 +1,16 @@
-import React, { useState, useEffect, useRef, } from 'react'
-import { View, AsyncStorage, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, AsyncStorage, BackHandler, ToastAndroid } from 'react-native'
 import Header from './Header'
 import Body from './Body'
-import DrawerLayout from 'react-native-gesture-handler/DrawerLayout';
-import { WIDTH } from '../../components/style';
 import Drawer from './Drawer';
 import Modal from 'react-native-modal';
 import BottomBannerAds from '../../components/View/BottomBannerAds';
+import { InterstitialAd } from '@react-native-firebase/admob';
+import { ADMOBEXIT } from '../../../secret';
+
+const exitIntersitial = InterstitialAd.createForAdRequest(ADMOBEXIT, {
+    requestNonPersonalizedAdsOnly: true,
+});
 
 const HomeScreen = () => {
 
@@ -25,7 +29,37 @@ const HomeScreen = () => {
 
     useEffect(() => {
         init()
+
+        exitIntersitial.load()
+
+        const backAction = () => {
+
+            ToastAndroid.show("Press again to exit", ToastAndroid.SHORT)
+            const backHandler2 = BackHandler.addEventListener(
+                "hardwareBackPress",
+                () => {
+                    BackHandler.exitApp()
+                    return true
+                }
+            )
+            setTimeout(() => {
+                backHandler2.remove()
+            }, 5000);
+
+            exitIntersitial.loaded && exitIntersitial.show()
+
+            return true;
+        };
+
+        const backHandler = BackHandler.addEventListener(
+            "hardwareBackPress",
+            backAction
+        );
+        return () => {
+            backHandler.remove();
+        };
     }, [])
+
 
     const setAutoPlayProcess = (value: boolean) => {
         setAutoplay(value)
